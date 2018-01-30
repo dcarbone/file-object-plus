3fcaa12d99673bef036a169a5af2964f37b0ad6e
Daniel Paul Carbone <dcarbone@ena.com>
2018-01-29 20:26:42 -0600

    Bumping minimum php version to 5.4, updating license from GPLv3 to MPL-2.0

--- tests/FileObjectPlus/FileObjectPlusTest.php
+++ tests/FileObjectPlus/FileObjectPlusTest.php
@@ -2,23 +2,17 @@
 
 /*
     Modified SplFileObject class that adds Countable interface and Pagination methods
-    Copyright (C) 2013-2015  Daniel Paul Carbone (daniel.p.carbone@gmail.com)
+    Copyright (C) 2013-2018  Daniel Paul Carbone (daniel.p.carbone@gmail.com)
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 /**
  * Class FileObjectPlusTest
  */
-class FileObjectPlusTest extends PHPUnit_Framework_TestCase
+class FileObjectPlusTest extends \PHPUnit\Framework\TestCase
 {
     /** @var \DCarbone\FileObjectPlus */
     protected $fileObject;
